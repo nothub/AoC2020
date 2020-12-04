4d5914495755643f3c1f22f0c54191529f7600ac
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-04 16:08:07 +0100

    day4 cleanup refactor

--- src/main/java/not/hub/aoc/day4/PassportProcessing1.java
+++ src/main/java/not/hub/aoc/day4/PassportProcessing1.java
@@ -3,7 +3,9 @@ package not.hub.aoc.day4;
 import not.hub.aoc.Solver;
 import org.tinylog.Logger;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PassportProcessing1 extends Solver<String, Integer> {
@@ -12,28 +14,21 @@ public class PassportProcessing1 extends Solver<String, Integer> {
 
     @Override
     public Integer solve(String input) {
-
         Logger.info("Day 4: Passport Processing - Part 1");
+        return (int) parsePassports(input).stream().filter(this::checkValidity).count();
+    }
 
-        int numValid = 0;
-
+    static List<Map<String, String>> parsePassports(String input) {
+        List<Map<String, String>> passports = new ArrayList<>();
         for (String raw : input.split("((" + linebreak + ")\\d*)+(" + linebreak + ")")) {
-
             Map<String, String> passport = new HashMap<>();
-
             for (String pair : raw.replaceAll(linebreak, " ").split(" ")) {
                 String[] data = pair.split(":");
                 passport.put(data[0], data[1]);
             }
-
-            if (checkValidity(passport)) {
-                numValid++;
-            }
-
+            passports.add(passport);
         }
-
-        return numValid;
-
+        return passports;
     }
 
     private boolean checkValidity(Map<String, String> passport) {
--- src/main/java/not/hub/aoc/day4/PassportProcessing2.java
+++ src/main/java/not/hub/aoc/day4/PassportProcessing2.java
@@ -3,37 +3,16 @@ package not.hub.aoc.day4;
 import not.hub.aoc.Solver;
 import org.tinylog.Logger;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import static not.hub.aoc.day4.PassportProcessing1.linebreak;
+import static not.hub.aoc.day4.PassportProcessing1.parsePassports;
 
 public class PassportProcessing2 extends Solver<String, Integer> {
 
     @Override
     public Integer solve(String input) {
-
         Logger.info("Day 4: Passport Processing - Part 2");
-
-        int numValid = 0;
-
-        for (String raw : input.split("((" + linebreak + ")\\d*)+(" + linebreak + ")")) {
-
-            Map<String, String> passport = new HashMap<>();
-
-            for (String pair : raw.replaceAll(linebreak, " ").split(" ")) {
-                String[] data = pair.split(":");
-                passport.put(data[0], data[1]);
-            }
-
-            if (checkValidity(passport)) {
-                numValid++;
-            }
-
-        }
-
-        return numValid;
-
+        return (int) parsePassports(input).stream().filter(this::checkValidity).count();
     }
 
     private boolean checkValidity(Map<String, String> passport) {
