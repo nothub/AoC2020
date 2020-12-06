ae0607ef0c55251dd93bebb7970c6c0c68c5a8fd
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-06 20:30:06 +0100

    IntStats

--- src/main/java/not/hub/aoc2020/day5/BinaryBoarding2.java
+++ src/main/java/not/hub/aoc2020/day5/BinaryBoarding2.java
@@ -3,6 +3,7 @@ package not.hub.aoc2020.day5;
 import not.hub.aoc2020.Solver;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static not.hub.aoc2020.day5.BinaryBoarding1.parse;
@@ -12,10 +13,8 @@ public class BinaryBoarding2 extends Solver<List<String>, Integer> {
     @Override
     public Integer solve(List<String> input) {
         var seatIds = parse(input);
-        return IntStream.range(
-                seatIds.stream().min(Integer::compareTo).orElseThrow(),
-                seatIds.stream().max(Integer::compareTo).orElseThrow()
-        ).filter(id -> !seatIds.contains(id)).findAny().orElseThrow();
+        var stats = seatIds.stream().collect(Collectors.summarizingInt(Integer::intValue));
+        return IntStream.range(stats.getMin(), stats.getMax()).filter(id -> !seatIds.contains(id)).findAny().orElseThrow();
     }
 
 }
