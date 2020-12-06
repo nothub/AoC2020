729a16e11461a379431ba683056764698e6f8157
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-06 16:43:33 +0100

    wrap as stream

--- src/main/java/not/hub/aoc2020/day6/CustomCustoms2.java
+++ src/main/java/not/hub/aoc2020/day6/CustomCustoms2.java
@@ -3,7 +3,6 @@ package not.hub.aoc2020.day6;
 import not.hub.aoc2020.Solver;
 
 import java.util.Arrays;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CustomCustoms2 extends Solver<String, Integer> {
@@ -12,17 +11,15 @@ public class CustomCustoms2 extends Solver<String, Integer> {
 
     @Override
     public Integer solve(String input) {
-        int result = 0;
-        Set<Character> answers = input.replaceAll(linebreak, "").chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
-        var a = Arrays.stream(input.replaceAll(linebreak, "\n").strip().split("\n{2,}")).collect(Collectors.toList());
-        for (String group : a) {
-            for (Character character : answers) {
-                if (group.chars().filter(ch -> ch == character).count() == group.chars().filter(ch -> ch == '\n').count() + 1) {
-                    result++;
-                }
-            }
-        }
-        return result;
+        var answers = input.replaceAll(linebreak, "")
+                .chars().mapToObj(c -> (char) c)
+                .collect(Collectors.toSet());
+        return Arrays.stream(input.replaceAll(linebreak, "\n").strip().split("\n{2,}"))
+                .collect(Collectors.toList()).stream().mapToInt(group ->
+                        (int) answers.stream().filter(character ->
+                                group.chars().filter(ch -> ch == character).count() ==
+                                        group.chars().filter(ch -> ch == '\n').count() + 1
+                        ).count()).sum();
     }
 
 }
