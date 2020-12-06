1fa4bdeb454403ac05f2364f96c2fccde1155ba4
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-06 20:18:52 +0100

    lul

--- src/main/java/not/hub/aoc2020/day5/BinaryBoarding2.java
+++ src/main/java/not/hub/aoc2020/day5/BinaryBoarding2.java
@@ -12,9 +12,10 @@ public class BinaryBoarding2 extends Solver<List<String>, Integer> {
     @Override
     public Integer solve(List<String> input) {
         var seatIds = parse(input);
-        return IntStream.range(0, seatIds.stream().max(Integer::compareTo).orElseThrow()).filter(
-                id -> !seatIds.contains(id) && seatIds.contains(id - 1) && seatIds.contains(id + 1)
-        ).findAny().orElseThrow();
+        return IntStream.range(
+                seatIds.stream().min(Integer::compareTo).orElseThrow(),
+                seatIds.stream().max(Integer::compareTo).orElseThrow()
+        ).filter(id -> !seatIds.contains(id)).findAny().orElseThrow();
     }
 
 }
