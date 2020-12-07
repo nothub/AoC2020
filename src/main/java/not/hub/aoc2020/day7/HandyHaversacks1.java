2a3d2ff09e419f577d75bca82254f3675d3ee862
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-07 16:58:35 +0100

    day7 cleanup

--- src/main/java/not/hub/aoc2020/day7/HandyHaversacks1.java
+++ src/main/java/not/hub/aoc2020/day7/HandyHaversacks1.java
@@ -14,9 +14,12 @@ import static not.hub.aoc2020.Globals.LB;
 public class HandyHaversacks1 extends Solver<String, Long> {
 
     protected static final Pattern pattern = Pattern.compile("^(.+) contain (.+)\\.$", Pattern.MULTILINE);
+    protected static final String shiny_gold = "shiny gold bag";
+    protected static final String empty = "no other bag";
 
     protected static boolean containsGold(String bag, Map<String, Map<String, Integer>> bags) {
-        return bag.equals("shiny gold bag") || bags.get(bag).entrySet().stream().anyMatch(inner -> containsGold(inner.getKey(), bags));
+        return bag.equals(shiny_gold) || bags.get(bag).entrySet().stream().anyMatch(inner ->
+                containsGold(inner.getKey(), bags));
     }
 
     protected static Map<String, Map<String, Integer>> parse(String input) {
@@ -29,7 +32,7 @@ public class HandyHaversacks1 extends Solver<String, Long> {
                     bags.put(name, new HashMap<>());
                 }
                 Set.of(matcher.group(2).split(", ")).forEach(rule -> {
-                    if (!rule.equals("no other bag")) {
+                    if (!rule.equals(empty)) {
                         bags.get(name).put(rule.substring(2), Integer.parseInt(rule.substring(0, 1)));
                     }
                 });
@@ -41,7 +44,9 @@ public class HandyHaversacks1 extends Solver<String, Long> {
     @Override
     public Long solve(String input) {
         var bags = parse(input);
-        return bags.keySet().stream().filter(name -> !name.equals("shiny gold bag")).filter(name -> containsGold(name, bags)).count();
+        return bags.keySet().stream().filter(name ->
+                !name.equals(shiny_gold)
+        ).filter(name -> containsGold(name, bags)).count();
     }
 
 }
--- src/main/java/not/hub/aoc2020/day7/HandyHaversacks2.java
+++ src/main/java/not/hub/aoc2020/day7/HandyHaversacks2.java
@@ -5,17 +5,19 @@ import not.hub.aoc2020.Solver;
 import java.util.Map;
 
 import static not.hub.aoc2020.day7.HandyHaversacks1.parse;
+import static not.hub.aoc2020.day7.HandyHaversacks1.shiny_gold;
 
 public class HandyHaversacks2 extends Solver<String, Integer> {
 
     @Override
     public Integer solve(String input) {
-        var bags = parse(input);
-        return innerBags("shiny gold bag", bags, false);
+        return innerBags(shiny_gold, parse(input), false);
     }
 
     private int innerBags(String name, Map<String, Map<String, Integer>> bags, boolean isInner) {
-        return bags.get(name).entrySet().stream().mapToInt(bag -> innerBags(bag.getKey(), bags, true) * bag.getValue()).sum() + (isInner ? 1 : 0);
+        return bags.get(name).entrySet().stream().mapToInt(bag ->
+                innerBags(bag.getKey(), bags, true) * bag.getValue()
+        ).sum() + (isInner ? 1 : 0);
     }
 
 }
