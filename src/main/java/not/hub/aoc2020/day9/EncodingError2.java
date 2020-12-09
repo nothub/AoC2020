79c977269595cc3268d2306032089c5fd1ab4406
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-09 20:57:40 +0100

    (╯°□°）╯︵ ┻━┻

--- src/main/java/not/hub/aoc2020/day9/EncodingError2.java
+++ src/main/java/not/hub/aoc2020/day9/EncodingError2.java
@@ -4,41 +4,26 @@ import not.hub.aoc2020.Solver;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static not.hub.aoc2020.day9.EncodingError1.calcNumber;
 
 
 public class EncodingError2 extends Solver<List<BigInteger>, BigInteger> {
 
-    private static BigInteger bruteforce(List<BigInteger> input, BigInteger number) {
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = i; j < input.size(); j++) {
-                Set<BigInteger> operands = new HashSet<>(input.subList(i, j));
-                if (operands.stream().reduce(BigInteger::add).orElse(BigInteger.ZERO).equals(number)) {
-                    return operands.stream().min(BigInteger::compareTo).orElseThrow().add(operands.stream().max(BigInteger::compareTo).orElseThrow());
-                }
-            }
-        }
-        throw new IllegalStateException("No result found");
-    }
-
-    public BigInteger d(List<BigInteger> input, int preambleSize) {
-        BigInteger part1 = calcNumber(input, preambleSize);
+    private static BigInteger bruteforce(List<BigInteger> input, BigInteger part1) {
         BigInteger result = null;
-        for (int i = 0; i < input.size(); i++) {
-            var test_num = input.get(i);
-            var sum = BigInteger.ZERO.add(test_num);
-            List<BigInteger> range = new ArrayList<>();
-            range.add(test_num);
-            for (int j = i + 1; j < input.size(); j++) {
-                var sum_num = input.get(j);
-                range.add(sum_num);
-                sum = sum.add(sum_num);
+        for (int n = 0; n < input.size(); n++) {
+            var numN = input.get(n);
+            var sum = BigInteger.ZERO.add(numN);
+            List<BigInteger> operands = new ArrayList<>();
+            operands.add(input.get(n));
+            for (int m = n + 1; m < input.size(); m++) {
+                var numM = input.get(m);
+                operands.add(numM);
+                sum = sum.add(numM);
                 if (sum.equals(part1)) {
-                    result = range.stream().min(BigInteger::compareTo).orElseThrow().add(range.stream().max(BigInteger::compareTo).orElseThrow());
+                    result = operands.stream().min(BigInteger::compareTo).orElseThrow().add(operands.stream().max(BigInteger::compareTo).orElseThrow());
                 } else if (sum.compareTo(part1) > 0) {
                     break;
                 }
@@ -52,8 +37,7 @@ public class EncodingError2 extends Solver<List<BigInteger>, BigInteger> {
 
     @Override
     public BigInteger solve(List<BigInteger> input, int preambleSize) {
-        return d(input, preambleSize);
-        //return bruteforce(input, calcNumber(input, preambleSize));
+        return bruteforce(input, calcNumber(input, preambleSize));
     }
 
     @Override
