2ee9cf1a3203be47daadeebf0febf437b71c40c1
blockparole <48992448+blockparole@users.noreply.github.com>
2020-12-09 21:31:21 +0100

    :ok_hand:

--- src/main/java/not/hub/aoc2020/day9/EncodingError2.java
+++ src/main/java/not/hub/aoc2020/day9/EncodingError2.java
@@ -3,49 +3,26 @@ package not.hub.aoc2020.day9;
 import not.hub.aoc2020.Solver;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.IntStream;
 
 import static not.hub.aoc2020.day9.EncodingError1.calcNumber;
 
 
 public class EncodingError2 extends Solver<List<BigInteger>, BigInteger> {
 
-    private static BigInteger optimized(List<BigInteger> input, BigInteger part1) {
-        AtomicReference<BigInteger> part1Atomic = new AtomicReference<>(part1);
-        AtomicReference<BigInteger> result = new AtomicReference<>();
-        for (int n = 0; n < input.size(); n++) {
-            AtomicReference<BigInteger> sum = new AtomicReference<>(BigInteger.ZERO.add(input.get(n)));
-            List<BigInteger> operands = new ArrayList<>();
-            operands.add(input.get(n));
-            IntStream.range(n + 1, input.size()).boxed().map(input::get).forEach(m -> {
-                operands.add(m);
-                sum.set(sum.get().add(m));
-                if (sum.get().equals(part1Atomic.get())) {
-                    result.set(operands.stream().min(BigInteger::compareTo).orElseThrow().add(operands.stream().max(BigInteger::compareTo).orElseThrow()));
-                }
-            });
-        }
-        if (result.get() == null) {
-            throw new IllegalStateException("No result found");
-        }
-        return result.get();
-    }
-
     public BigInteger bruteforce(List<BigInteger> input, BigInteger part1) {
         Set<BigInteger> range = null;
         for (int i = 0; i < input.size(); i++) {
-            BigInteger sum = BigInteger.ZERO.add(input.get(i));
-            Set<BigInteger> operands = new HashSet<>();
-            operands.add(input.get(i));
-            for (int j = i; j < input.size(); j++) {
-                operands.add(input.get(j));
-                if (operands.stream().reduce(BigInteger::add).orElse(BigInteger.ZERO).equals(part1)) {
-                    range = operands;
+            BigInteger contiguousSum = BigInteger.ZERO.add(input.get(i));
+            Set<BigInteger> contiguousSet = new HashSet<>();
+            contiguousSet.add(input.get(i));
+            for (int j = i + 1; j < input.size(); j++) {
+                contiguousSum = contiguousSum.add(input.get(j));
+                contiguousSet.add(input.get(j));
+                if (contiguousSum.equals(part1)) {
+                    range = contiguousSet;
                     break;
                 }
             }
@@ -61,14 +38,7 @@ public class EncodingError2 extends Solver<List<BigInteger>, BigInteger> {
 
     @Override
     public BigInteger solve(List<BigInteger> input, int preambleSize) {
-        var b = bruteforce(input, calcNumber(input, preambleSize));
-        var o = optimized(input, calcNumber(input, preambleSize));
-        if (!b.equals(o)) {
-            throw new IllegalStateException("Implementation broken!");
-        }
-        return o;
-        //return bruteforce(input, calcNumber(input, preambleSize));
-        //return optimized(input, calcNumber(input, preambleSize));
+        return bruteforce(input, calcNumber(input, preambleSize));
     }
 
     @Override
